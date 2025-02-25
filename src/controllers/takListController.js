b325fb78ef5229e61dee0a2bfb3347ddb8416d0c
Gabriel Goldas <gabriel.goldas@gmail.com>
2025-02-25 19:56:54 -0300

    first commit with first route to index view

---
+++ src/controllers/takListController.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // GET /app
+
+  // GET /app/nova-lista
+
+  // POST/app/nova-lista
+
+  // GET /app/:id
+
+  // POST /app/:id/excluir
+
+  // POST /app/:id/nova-tarefa
+
+  // POST /app/:listId/completar/:taskId
+}
